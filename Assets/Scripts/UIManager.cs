eeb2d6c90b1741d8c37262fbde48a5a1140f69f8
jrevelldev <jrevell@cifog.cat>
2025-08-07 18:40:23 +0200

    Add score and high score tracking with UI separation
    Implement score system with PlayerPrefs storage and fixed label/value layout

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -6,7 +6,13 @@ public class UIManager : MonoBehaviour
     public static UIManager Instance { get; private set; }
 
     [SerializeField] private TextMeshProUGUI messageText;
-    [SerializeField] private GameUITextData uiTextData; // ← Add this
+    [SerializeField] private GameUITextData uiTextData;
+
+    [SerializeField] private TextMeshProUGUI scoreValueText;
+    [SerializeField] private TextMeshProUGUI highScoreValueText;
+
+    private int score = 0;
+    private int highScore = 0;
 
     void Awake()
     {
@@ -15,6 +21,37 @@ public class UIManager : MonoBehaviour
         else
             Destroy(gameObject);
     }
+
+    void Start()
+    {
+        highScore = PlayerPrefs.GetInt("HighScore", 0);
+        UpdateUI();
+    }
+
+    public void AddScore(int amount)
+    {
+        score += amount;
+        if (score > highScore)
+        {
+            highScore = score;
+            PlayerPrefs.SetInt("HighScore", highScore);
+        }
+
+        UpdateUI();
+    }
+
+    void UpdateUI()
+    {
+        scoreValueText.text = score.ToString();
+        highScoreValueText.text = highScore.ToString();
+    }
+
+    public void ResetScore()
+    {
+        score = 0;
+        UpdateUI();
+    }
+
 
     void Update()
     {
@@ -35,5 +72,17 @@ public class UIManager : MonoBehaviour
         {
             GameManager.Instance.ResetGame();
         }
+
+        if (GameManager.Instance.CurrentState == GameManager.GameState.Playing && Input.GetKeyDown(KeyCode.S))
+        {
+            AddScore(1);
+        }
+
+        if (GameManager.Instance.CurrentState == GameManager.GameState.Dead && Input.GetKeyDown(KeyCode.R))
+        {
+            ResetScore();
+            GameManager.Instance.ResetGame();
+        }
+
     }
 }
